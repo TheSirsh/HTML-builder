d45b253e34503faf6d7ae7ac5b93e0b78bb0d328
TheSirsh <sirsh@bk.ru>
2022-11-01 18:01:14 +0300

    upd: add color to task 1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,13 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const doc = path.join( __dirname, "text.txt");
 const { stdout } = require('process');
-const textStream = fs.createReadStream(doc)
+const textStream = fs.createReadStream(doc);
+const chalk = require('chalk');
 
 textStream.on('readable', readFile)
 
 function readFile() {
   let textFile = textStream.read();
   if(textFile != null) {
-    stdout.write(textFile);
+    stdout.write(chalk.green(textFile));
   }
 }
